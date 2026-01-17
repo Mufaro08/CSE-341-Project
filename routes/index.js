9e3895a9aaa7d0bb15b74240800a771303d6d665
Mufaro <mufaromaz@gmail.com>
2026-01-18 01:31:37 +0200

    project1

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,6 @@ const router = require(`express`).Router();
 
 router.get(`/`, (req, res) => { res.send(`Welcome to the Home Page!`); });
 
+router.use(`/users`, require(`./users`));
+
 module.exports = router;
