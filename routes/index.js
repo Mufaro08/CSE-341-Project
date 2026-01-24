2a514b50ce960580e9792b2c81380859395c6df7
Mufaro <mufaromaz@gmail.com>
2026-01-24 05:57:48 +0200

    installed swagger

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require(`express`).Router();
+router.use(`/`, require(`./swagger`));
 
-router.get(`/`, (req, res) => { res.send(`Welcome to the Home Page!`); });
+router.get(`/`, (req, res) => {
+    //swagger.tags= ['Welcome to the Home Page!'];
+    res.send(`Welcome to the Home Page!`);
+});
 
 router.use(`/users`, require(`./users`));
 
